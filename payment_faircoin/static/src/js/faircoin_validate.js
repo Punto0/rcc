decacf5dd4f1e3c8b7a829de25afa62f3463383d
Test Compras Colectivas <rcc@vps311858.ovh.net>
2016-11-14 18:02:54 +0100

    Bugfix last commit

--- payment_faircoin/static/src/js/faircoin_validate.js
+++ payment_faircoin/static/src/js/faircoin_validate.js
@@ -21,8 +21,8 @@ $(document).ready(function () {
             else {
                 txt = result.message;
                 txt = result.state + "<h2>Thank you, your transaction has been confirmed.</h2><p>Your partner will contact with you as soon as posible</p>";
+                window.location.replace("http://rcc.punto0.org:8069/shop/payment/validate");
             }
-            window.location.replace("http://rcc.punto0.org:8069/shop/payment/validate");
             tx_node.html(txt);
         });
     }
